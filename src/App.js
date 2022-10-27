2491a9ea4734b72ca76da70b04712c46bb440142
jiongxie <byeonhs0018@naver.com>
2022-10-27 10:23:03 +0900

    Create Customer.js, map

--- src/App.js
+++ src/App.js
@@ -4,22 +4,53 @@ import React, {Component } from 'react';
 import Customer from './components/Customer';
 
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '이순신',
+    'birthday': '461020',
+    'gender': '남자',
+    'job': '군인'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '하루',
+    'birthday': '031201',
+    'gender': '여자',
+    'job': '고등학생'
+  }
+  ]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+            <Customer
+              key={c.id} // map 키값 줘야됨
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            );
+          })
+        }
+      </div>  
     );
   }
 }
